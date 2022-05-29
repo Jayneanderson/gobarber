8a87ca715a37c2f85eed00ca0d0f928bb460ec60
Jayneanderson <aprenderprogramacao001@gmail.com>
2022-05-29 01:02:08 -0300

    build: add database configuration file

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+//se der pau use module.exports =
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'docker',
+    database: 'gobarder',
+    define: {
+        timestamps: true,//tabelas created_at e update_at,
+        underscored: true, //forma de criar as tabelas no formato snake_case
+        underscoredAll: true,
+
+    },
+};
