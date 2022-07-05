41de2721331b9eeb4aed6bde03fd006525fd4f2a
Jayneanderson <aprenderprogramacao001@gmail.com>
2022-07-04 21:51:14 -0300

    feat: create route to save user

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,10 @@
 // constar o Router para separar a parte de roteamento do express
 const { Router } = require('express');
-const User = require('./app/models/User.js');
+const UserController = require('./app/controllers/UserController.js');
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'Jay Show',
-        email: 'jayneanderson@gupy.com.br',
-        password_hash: '12345612',
-    });
-    return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 // module.exports = routes;
 module.exports = routes;
