32a31ef13038a1f2ff57b6c39a4a803fcb1d916b
Jayneanderson <aprenderprogramacao001@gmail.com>
2022-07-09 00:22:16 -0300

    feat: add validation when creating session

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,20 +1,31 @@
 
 const jwt = require('jsonwebtoken');
+const Yup = require('yup');
 const authConfig = require('../../config/auth.js');
 const User = require('../models/User.js');
 
 class SessionController {
     //vamos pegar a senha e o e-mail
     async store(req, res) {
+        const schema = Yup.object().shape({
+            email: Yup.string().email().required(),
+            password: Yup.string().required(),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
+
+
         const { email, password } = req.body;
+        
+        const user = await User.findOne({ where: {email: email} });
 
-        const user = await User.findOne({ where: {email: email }});
-
-        if (!user) {
+        if (!user || user == undefined) {
             return res.json(401).json({ error: 'User not found' });
         }
 
-        if (!await user.checkPassword(password)) {
+        if (!(await user.checkPassword(password))) {
             return res.status(401).json({ erro: 'Password does not match' });
         }
 
