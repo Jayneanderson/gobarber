c8334bc6d0e26fcca4236ae3f9b590785bc6bdd2
Jayneanderson <aprenderprogramacao001@gmail.com>
2022-07-13 23:25:56 -0300

    feat(rout,config): finished featyre upload file in folder

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 // constar o Router para separar a parte de roteamento do express
 const { Router } = require('express');
+const multer = require('multer');
+const multerConfig = require('./config/multer.js');
+
 const UserController = require('./app/controllers/UserController.js');
 const SessionController = require('./app/controllers/SessionController.js');
 const authMiddlware = require('./app/middlewares/auth.js');
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -13,6 +17,10 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddlware);
 
 routes.patch('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => { 
+    return res.json({ ok: true });
+});
 
 // module.exports = routes;
 module.exports = routes;
