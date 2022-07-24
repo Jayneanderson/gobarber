4a0bac1876e96d5c9a929e9a900c7f62e6450e57
Jayneanderson <aprenderprogramacao001@gmail.com>
2022-07-23 21:39:27 -0300

    feat: add function to serve static files

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // este arquivo serve para separar a criação da estrutura da aplicação para facilitar no futuro (ex: testes)
 // const express = require('express'); //é o mesmo que abaixo
 const express = require ('express');
+const path = require('path');
 const routes = require('./routes.js');
 require('./database/index.js');
 
@@ -14,6 +15,9 @@ class App {
 
     middlewares() {
         this.server.use(express.json());
+
+        //serve para servir arquivos estáticos
+        this.server.use('/files', express.static(path.resolve(__dirname, '..', 'temp', 'uploads')));
     }
 
     routes() {
