60edf988520b11c723789edef882aad605ed3a0b
Jayneanderson <aprenderprogramacao001@gmail.com>
2022-07-23 21:35:33 -0300

    feat(route) add providers route

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ const UserController = require('./app/controllers/UserController.js');
 const SessionController = require('./app/controllers/SessionController.js');
 const authMiddlware = require('./app/middlewares/auth.js');
 const FileController = require('./app/controllers/FileController.js');
+const ProviderController = require('./app/controllers/ProviderController.js');
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -19,7 +20,7 @@ routes.use(authMiddlware);
 
 routes.patch('/users', UserController.update);
 
-routes.patch('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.fileUpload);
 
